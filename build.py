13cb914a8e1639a37ea7b111f1bcd5d46c3b84e6
Uilian Ries <uilian@khomp.com>
2017-10-09 08:47:05 -0300

    #111 Avoid debug build
    - Skipped x86 and debug build
    
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="macchina.io:shared")
+    builder.add_common_builds(shared_option_name="macchina.io:shared", pure_c=False)
     builder.run()
