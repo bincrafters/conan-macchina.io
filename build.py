1f9a604f3e15b1dd8031cd38be9565c97e1e48d1
Uilian Ries <uilianries@gmail.com>
2017-10-12 15:20:41 -0300

    #111 Removed ARM build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -6,11 +6,6 @@ if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
 
-    builder.add(settings={"arch": "armv7", "build_type": "Release", "compiler": "gcc", "compiler.version": "6.3", "compiler.libcxx": "libstdc++"},
-                options={"macchina.io:V8_snapshot": False},
-                env_vars={"CC": "arm-linux-gnueabi-gcc", "CXX": "arm-linux-gnueabi-g++"},
-                build_requires={})
-
     # V8 snapshot only works on gcc-4
     filtered_builds = []
     for settings, options, env_vars, build_requires in builder.builds:
